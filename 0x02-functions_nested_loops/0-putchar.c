d0064ada11a530729f68fb4c05aa7104cd5122d3
shia22 <shelvinmoraa@gmail.com>
2022-03-16 22:36:42 +0000

    Commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,20 @@
 #include "main.h"
 /**
- * main - prints _put
+ * main - Putchar
  *
- * Return: 0 (Success)
+ * _putchar prints the chracter given
+ * Return: Always 0 (Success)
  */
-
-int main(void) {
-	char *ch = "_putchar";
-	while (*ch)
-	{
-		_putchar(*ch);
-		ch++;
-	}
-	_putchar('\n'); return (0);
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
