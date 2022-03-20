29fb68e90eaa913fc35bbb8cf1ad1cd4fefdfeb4
shia22 <shelvinmoraa@gmail.com>
2022-03-20 12:32:38 +0000

    debugging commit

---
+++ main.h
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Tests a function that checks whether integer
+ * is positive or negative
+ * Return: void
+ */
+
+int main(void)
+{
+	int i;
+
+
+	i = 0;
+	positive_or_negative(i);
+
+
+	return (0);
+}
