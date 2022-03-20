670bee0d5ee294ba6695614c9a5a53f228292b5a
Shia22 <99464993+Shia22@users.noreply.github.com>
2022-03-20 22:41:05 +0300

    2-mul.c

---
+++ 2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - int function
+ * @a: function variable
+ * @b: function variable
+ * Return: val
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+	_putchar('\n');
+}
