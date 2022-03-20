48284097fc76f6377d1167516a09efb168683b56
Shia22 <99464993+Shia22@users.noreply.github.com>
2022-03-20 22:36:34 +0300

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - int function
+ * @c: function variable
+ * Return: 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
