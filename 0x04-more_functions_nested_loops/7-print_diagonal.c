a65747cd730e20c748cc214a09512759db069ece
Shia22 <99464993+Shia22@users.noreply.github.com>
2022-03-20 23:11:25 +0300

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - void function
+ * @n: number of lines
+ */
+
+void print_diagonal(int n)
+{
+	int l, m;
+
+	if (n <= 0)
+		_putchar('\n');
+
+	for (l = 0; l < n; l++)
+	{
+		for (m = 0; m <= l; m++)
+		{
+			if (l == m)
+			{
+				_putchar('\\');
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+	}
+}
